5013a6790510fa4513e58197a65b0ecd5f1bc052
Richard Mills <scripts-richard@gmail.com>
2024-01-02 12:40:27 -0800

    add rtx and android configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,14 @@
-set ANDROID_HOME $HOME/Android/Sdk
+set -x ANDROID_HOME $HOME/Android/Sdk
+set -x JAVA_HOME /usr/lib/jvm/java-17-openjdk/
+set -x FASTLANE_USER rmills@quotetome.com
+
 set PATH $PATH $ANDROID_HOME/tools
 set PATH $PATH $ANDROID_HOME/platform_tools
 set PATH $PATH $HOME/.cargo/bin/
 set PATH $HOME/.node_modules/bin $PATH
 
-bass source /usr/share/nvm/init-nvm.sh
-
 starship init fish | source
+rtx activate fish | source
 
 alias ls="exa -1"
 alias ll="exa -l"
