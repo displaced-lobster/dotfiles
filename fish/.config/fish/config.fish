164723525fc86065d01b8e271b053ae6e9dfb692
Richard Mills <scripts-richard@gmail.com>
2024-11-01 16:03:00 -0700

    setup wayland

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ set -x ANDROID_HOME $HOME/Android/Sdk
 set -x JAVA_HOME /usr/lib/jvm/java-17-openjdk/
 set -x FASTLANE_USER rmills@quotetome.com
 
+set -x ELECTRON_OZONE_PLATFORM_HINT auto
+
 set PATH $PATH $ANDROID_HOME/tools
 set PATH $PATH $ANDROID_HOME/platform_tools
 set PATH $PATH $HOME/.cargo/bin/
